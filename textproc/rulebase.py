7d9633f881b6ba4144e214f27da34bf0953f90d0
taiyoslime <t@iyosli.me>
2020-11-17 02:21:16 +0900

    implement and improve some funcs for text processing

--- textproc/rulebase.py
+++ textproc/rulebase.py
@@ -1,16 +1,13 @@
-from . import utils
+if __package__ is None or __package__ == '':
+	import utils
+else:
+	from . import utils
 import re
+import spacy
 
 def desmasu2dadearu(text):#ですますをだ・であるに変換
-    """
-    wakati,dic= utils.wakatize(text)
-    for i in range(len(wakati)):
-        if wakati[i] in ["です","ます"]:
-            wakati[i]={"です":"だ","ます":"た"}[wakati[i]]
-    return ''.join(wakati)
-    """
-    chikan_list=[['しましょう','しよう'],
-            ['きましょう','こう'],
+	chikan_list=[['しましょう','しよう'],
+			['きましょう','こう'],
 			['りましょう','ろう'],
 			['出来ました','出来た'],
 			['できました','できた'],
@@ -42,19 +39,22 @@ def desmasu2dadearu(text):#ですますをだ・であるに変換
 			['ります','る'],
 			['ます','る'],
 			['です','だ']]
-    for chikan in chikan_list:
-        text=text.replace(chikan[0],chikan[1])
-    return text
+	for chikan in chikan_list:
+		text=text.replace(chikan[0],chikan[1])
+	return text
 
 def taigendomize(text):
-    wakati, dic = utils.wakatize(text)
-    idx = 0
-    for i, token in enumerate(reversed(dic)):
-        pos, *_ = token.part_of_speech.split(",")
-        if pos == "名詞":
-            idx = i
-            break
-    return "".join(wakati[:-idx])
+	wakati, dic = utils.wakatize(text)
+	if dic[-1].base_form != "名詞" and wakati[-1] == "か":
+		return text
+	idx = 0
+	for i, token in enumerate(reversed(dic)):
+		pos, *_ = token.part_of_speech.split(",")
+		base_form = token.base_form
+		if pos == "名詞" or base_form == "ない":
+			idx = i
+			break
+	return "".join(wakati[:-idx]) if idx != 0 else text
 
 def sanitize(_doc):
 	doc = _doc.copy()
@@ -62,7 +62,39 @@ def sanitize(_doc):
 		doc[i] = e.strip()
 	return sorted(set(doc), key=doc.index)
 
+def simplify(text):
+	text = text.split('、')
+	return text[-1]	
 
-def tiltlize(text):
-	text = re.sub('[、。]', '', text)
+def titleize(text, is_extract_subject=True):
+	text = simplify(text)
+	text = taigendomize(text)
+	text = desmasu2dadearu(text)
+	if is_extract_subject:
+		text = extract_subject(text)
 	return text
+
+def extract_subject(text):
+	wakati, subject = detect_subject_idx(text)
+	print(subject)
+	if subject == []:
+		return text
+	else:
+		border = subject[-1]
+		return "".join(wakati[:border + 1])
+
+def detect_subject_idx(text):
+	parser = spacy.load("ja_ginza")
+	doc = parser(text)
+	tokens = []
+	for sent in doc.sents:
+		for token in sent:
+			tokens.append(token)
+	wakati = []
+	subject = []
+	for token in tokens:
+		wakati.append(token.orth_)
+		if token.dep_ in ["nsubj", "nsubjpass", "iobj"]:
+			subject.append(token.i)
+	return wakati, subject
+
